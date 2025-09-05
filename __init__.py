b5467b3d774cde4ae0516712d99bd84170b72aba
borschtgonzales <lucien.richter02@gmail.com>
2025-09-05 11:39:43 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,3 @@
-#-----------------------------------------------------------
-# Copyright (C) 2015 Martin Dobias
-#-----------------------------------------------------------
-# Licensed under the terms of GNU GPL 2
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#---------------------------------------------------------------------
-
 from PyQt5.QtWidgets import QAction, QMessageBox
 
 def classFactory(iface):
